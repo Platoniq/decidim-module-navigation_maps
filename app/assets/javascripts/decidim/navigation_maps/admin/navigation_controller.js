cb30390ed7c087c9b3ec62d7f918cb53aa129763
David Igón <david.igon@riseup.net>
2019-10-23 14:03:29 +0200

    change javascript for navigation_controller

--- app/assets/javascripts/decidim/navigation_maps/admin/navigation_controller.js
+++ app/assets/javascripts/decidim/navigation_maps/admin/navigation_controller.js
@@ -4,83 +4,25 @@
 //= require leaflet-geoman.min
 //= require_self
 
-var image = new Image();
-image.src = '/uploads/decidim/navigation_map_homepage_content_block/map_image/11/BASE_navegador_081019.jpg';
-
-
-var draw = [
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [515,692.5166625976562],
-            [552, 728.5166625976562],
-            [1005, 250.51666259765625],
-            [970, 216.62916564941406],
-            [888.75, 302.62916564941406],
-            [872.25, 287.37916564941406],
-            [810, 354.7583312988281],
-            [806, 351.7583312988281],
-            [711, 452.2583312988281],
-            [699, 443.2583312988281],
-            [672.5, 470.7583312988281],
-            [652, 449.7583312988281],
-            [625, 480.7583312988281],
-            [645, 499.7583312988281],
-            [632, 514.2583312988281],
-            [659.5, 541.2583312988281],
-            [515, 692.5166625976562]
-          ]
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [1078,106.19999694824219],
-            [1112,66.19999694824219],
-            [1257,202.1999969482422],
-            [1255,273.1999969482422],
-            [1078,106.19999694824219]
-          ]
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [677,656.1999969482422],
-            [768,763.1999969482422],
-            [710,816.1999969482422],
-            [613,709.1999969482422],
-            [677,656.1999969482422]
-          ]
-        ]
-      }
-    }
-  ];
-
-var map = L.map('map', {
+$(function() {
+  var image = new Image();
+  image.src = $('#map').data('map');
+  
+  var map = L.map('map', {
+    minZoom: -1,
+    maxZoom: 2,
     crs: L.CRS.Simple,
-    center: [image.width/2, image.height/2],
-    zoom: 0,
+    center: [image.height/2,image.width/2],
+    zoom: -1,
+  });
+  
+  var bounds = [[0,0], [image.height,image.width]];
+  L.imageOverlay(image.src, bounds).addTo(map);
+  var blueprint = $('#map').data('blueprint');
+  for (area in blueprint) {
+    var geoarea = blueprint[area];
+    new L.GeoJSON(geoarea).addTo(map);
+  }
+  
+  map.invalidateSize();
 });
-
-var bounds = [[0,0], [image.height,image.width]];
-L.imageOverlay(image.src, bounds).addTo(map);
-L.geoJSON(draw).addTo(map);
-map.fitBounds(bounds);
-
-map.invalidateSize();
-
--- app/assets/javascripts/decidim/navigation_maps/admin/navigation_controller_settings_form_cell.js
+++ app/assets/javascripts/decidim/navigation_maps/admin/navigation_controller_settings_form_cell.js
@@ -5,18 +5,18 @@
 //= require_self
 
 var image = new Image();
-image.src = '/uploads/decidim/navigation_map_homepage_content_block/map_image/11/BASE_navegador_081019.jpg'
+image.src = $('#map').data('map');
 
 var map = L.map('map', {
+    minZoom: -1,
+    maxZoom: 2,
     crs: L.CRS.Simple,
-    center: [0,0],
-    zoom: 1
+    center: [image.height/4, image.width/2],
+    zoom: -1
 });
-
 
 var bounds = [[0,0], [image.height,image.width]];
 L.imageOverlay(image.src, bounds).addTo(map);
-map.fitBounds(bounds);
 
 map.pm.addControls({
   position: 'topleft',
@@ -29,15 +29,23 @@ map.invalidateSize();
 var features = [];
 
 map.on('pm:create', e => {
-  features.push(e.layer.toGeoJSON());
+  features[e.layer._leaflet_id] = e.layer.toGeoJSON();
+  e.layer.on('pm:edit', e => {
+    features[e.target._leaflet_id] = e.target.toGeoJSON();
+  });
 });
 
 map.on('pm:remove', e => {
   features.pop(e.layer.toGeoJSON());
 });
 
-$('#leaflet-save').click(function() {
+$('#leaflet-save').click(function(e) {
+  e.preventDefault();
   console.info(features);
-  return features;
+  $.post({
+    url: '/admin/navigation_maps/blueprints',
+    data: {blueprint: features},
+    dataType: "json"
+   });
 });
 
