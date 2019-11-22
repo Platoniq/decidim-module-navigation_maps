ec51eaf5a8631753af73242b34c153b15b6089bb
Ivan Vergés <ivan@platoniq.net>
2019-11-22 11:01:24 +0100

    map editor refactor

---
+++ app/assets/javascripts/decidim/navigation_maps/admin/map_editor.js
@@ -0,0 +1,132 @@
+// Creates a map
+
+function MapEditor(map_object, table_object) {
+  var self = this;
+  self.features = {};
+  self.map_object = map_object;
+  self.table_object = table_object;
+  self.id = map_object.dataset.id;
+  self.image_path = map_object.dataset.image;
+  self.blueprint = map_object.dataset.blueprint;
+  self.blueprint = self.blueprint ? JSON.parse(self.blueprint) : {};
+  self.image = new Image();
+  self.image.onload = function() {
+    self.createMap();
+  }
+  self.image.src = self.image_path;
+}
+
+MapEditor.prototype.createMap = function() {
+  var self = this;
+  self.map = L.map(self.map_object, {
+      minZoom: -1,
+      maxZoom: 2,
+      crs: L.CRS.Simple,
+      center: [self.image.height/2, self.image.width/2],
+      zoom: -1
+  });
+
+  var bounds = [[0,0], [self.image.height,self.image.width]];
+  L.imageOverlay(self.image.src, bounds).addTo(self.map);
+
+  self.map.pm.addControls({
+    position: 'topleft',
+    drawCircle: false,
+    drawMarker: false,
+    drawCircleMarker: false,
+    drawPolyline: false,
+    cutPolygon: false
+  });
+
+  if(self.blueprint) {
+    self.createAreas();
+  }
+
+  self.map.on('pm:create', function(e) {
+    var geojson = e.layer.toGeoJSON();
+    self.blueprint[e.layer._leaflet_id] = geojson;
+    self.renderRow(e.layer, geojson);
+  });
+
+  self.map.on('pm:remove', function(e) {
+    delete self.blueprint[e.layer._leaflet_id];
+    document.getElementById(self.rowId(e.layer._leaflet_id)).remove();
+  });
+
+};
+
+MapEditor.prototype.createAreas = function() {
+  var self = this;
+  for (area in self.blueprint) {
+    var geoarea = self.blueprint[area];
+    // avoid non-polygons for the moment
+    if(!geoarea.geometry || geoarea.geometry.type !== 'Polygon') continue;
+
+    new L.GeoJSON(geoarea, {
+      onEachFeature: function(feature, layer) {
+        console.log('add feature', feature, layer);
+        layer._leaflet_id = area;
+        self.renderRow(layer, feature);
+      }
+    }).addTo(self.map);
+  }
+};
+
+MapEditor.prototype.renderRow = function (layer, feature) {
+  var self = this;
+  var tbody = self.table_object.getElementsByTagName('tbody')[0];
+  var tr = this.rowTemplate(layer._leaflet_id, feature);
+
+  layer.on('mouseover', function(e) {
+    e.target.getElement().classList.add('selected')
+    document.getElementById(self.rowId(e.target._leaflet_id)).classList.add('selected');
+  });
+
+  layer.on('mouseout', function(e) {
+    e.target.getElement().classList.remove('selected')
+    document.getElementById(self.rowId(e.target._leaflet_id)).classList.remove('selected');
+  });
+
+  tr.addEventListener("mouseover", function() {
+    layer.getElement().classList.add('selected')
+  });
+
+  tr.addEventListener("mouseout", function() {
+    layer.getElement().classList.remove('selected')
+  });
+
+  layer.on('pm:edit', function(e) {
+    self.blueprint[e.target._leaflet_id] = e.target._leaflet_id;
+    // var tr = document.getElementById(self.rowId(e.target._leaflet_id));
+    // tr.parentNode.replaceChild(self.rowTemplate(e.target._leaflet_id, self.blueprint[e.target._leaflet_id]), tr);
+  });
+
+  tbody.appendChild(tr);
+};
+
+MapEditor.prototype.removeRow = function (area, feature) {
+
+};
+
+MapEditor.prototype.rowTemplate = function (area, feature) {
+  var link = feature.properties && feature.properties.link || '#';
+  var tr = document.createElement('tr');
+  tr.id = this.rowId(area);
+  tr.innerHTML = ' <td>' + area +'</td>'
+               + ' <td><input type="text" id="' + tr.id + '-link" value="' + link + '"></td>';
+  return tr;
+};
+
+MapEditor.prototype.rowId = function (id) {
+  return `map-editor-${this.id}-tr-${id}`;
+};
+
+MapEditor.prototype.getBlueprint = function () {
+  var self = this;
+  Object.keys(self.blueprint).forEach(function(key) {
+    var link = document.getElementById(self.rowId(key) + "-link");
+    self.blueprint[key].properties = self.blueprint[key] && self.blueprint[key].properties || {link: '#'}
+    self.blueprint[key].properties.link = link.value;
+  });
+  return self.blueprint;
+};
--- app/assets/javascripts/decidim/navigation_maps/admin/navigation_maps.js
+++ app/assets/javascripts/decidim/navigation_maps/admin/navigation_maps.js
@@ -3,145 +3,8 @@
 //= require leaflet
 //= require leaflet-geoman.min
 //= require jquery.form
+//= require decidim/navigation_maps/admin/map_editor
 //= require_self
-
-function MapEditor(image_path, blueprint) {
-  var self = this;
-  self.features = {};
-  self.blueprint = blueprint || {};
-  self.image = new Image();
-  self.image.onload = function() {
-    self.createMap();
-  }
-  self.image.src = image_path;
-  self.defaultStyle = {
-    color: "#2262CC",
-    weight: 3,
-    opacity: 0.6,
-    fillOpacity: 0.1,
-    fillColor: "#2262CC"
-  };
-  self.highlightStyle = {
-    color: '#2262CC',
-    weight: 3,
-    opacity: 0.6,
-    fillOpacity: 0.65,
-    fillColor: '#2262CC'
-  };
-}
-
-MapEditor.prototype.createMap = function() {
-  var self = this;
-  self.map = L.map('navigation_maps-map', {
-      minZoom: -1,
-      maxZoom: 2,
-      crs: L.CRS.Simple,
-      center: [self.image.height/2, self.image.width/2],
-      zoom: -1
-  });
-
-  var bounds = [[0,0], [self.image.height,self.image.width]];
-  L.imageOverlay(self.image.src, bounds).addTo(self.map);
-
-  self.map.pm.addControls({
-    position: 'topleft',
-    drawCircle: false,
-    drawMarker: false,
-    drawCircleMarker: false,
-    drawPolyline: false,
-    cutPolygon: false
-  });
-
-  if(self.blueprint) {
-    self.createAreas();
-  }
-
-  self.map.on('pm:create', function(e) {
-    var geojson = e.layer.toGeoJSON();
-    self.blueprint[e.layer._leaflet_id] = geojson;
-    self.renderTr(e.layer._leaflet_id, geojson);
-    e.layer.on('pm:edit', function(e) {
-      var area = e.target._leaflet_id;
-      self.blueprint[area] = e.target.toGeoJSON();
-      $('#leaflet-tr-' + area).replaceWith(self.getTr(area, self.blueprint[area]));
-    });
-    e.layer.on('mouseover', function(e) {
-      e.target.setStyle(self.highlightStyle);
-      $('#leaflet-tr-' + e.target._leaflet_id).addClass('selected');
-    });
-    e.layer.on('mouseout', function(e) {
-      e.target.setStyle(self.defaultStyle);
-      $('#leaflet-tr-' + e.target._leaflet_id).removeClass('selected');
-    });
-  });
-
-  self.map.on('pm:remove', function(e) {
-    delete self.blueprint[e.layer._leaflet_id];
-    document.getElementById('leaflet-tr-' + e.layer._leaflet_id).remove();
-  });
-
-};
-
-MapEditor.prototype.createAreas = function() {
-  var self = this;
-  for (area in self.blueprint) {
-    var geoarea = self.blueprint[area];
-    if(!geoarea.geometry || geoarea.geometry.type !== 'Polygon') continue;
-
-    new L.GeoJSON(geoarea, {
-      onEachFeature: function(feature, layer) {
-        layer._leaflet_id = area;
-        self.renderTr(area, feature);
-        layer.on('pm:edit', function(e) {
-          self.blueprint[area] = e.target.toGeoJSON();
-        });
-        layer.on('mouseover', function(e) {
-          layer.setStyle(self.highlightStyle);
-          $('#leaflet-tr-' + layer._leaflet_id).addClass('selected');
-        });
-        layer.on('mouseout', function(e) {
-          layer.setStyle(self.defaultStyle);
-          $('#leaflet-tr-' + layer._leaflet_id).removeClass('selected');
-        });
-      }
-    }).addTo(self.map);
-  }
-};
-
-MapEditor.prototype.renderTr = function (area, feature) {
-  var self = this;
-  var tr = $('#leaflet-body').append(this.getTr(area, feature))
-  $('#leaflet-tr-'+area).mouseover(function() { self.highlightArea(area) });
-  $('#leaflet-tr-'+area).mouseout(function() { self.defaultArea(area) });
-};
-
-MapEditor.prototype.getTr = function (area, feature) {
-  var link = feature.properties && feature.properties.link || '#';
-  return '<tr id="leaflet-tr-' + area + '" data-id="' + area + '"> '
-    + ' <td id="leaflet-td-' + area + '-id">' + area +'</td>'
-    + ' <td id="leaflet-td-' + area + '-type">' + feature.geometry.type +'</td>'
-    + ' <td id="leaflet-td-' + area + '-link"> <input type="text" id="leaflet-feature-link-' + area + '" value="' + link + '"> </td>'
-    + '</tr>';
-};
-
-MapEditor.prototype.getBlueprint = function () {
-  var self = this;
-  Object.keys(self.blueprint).forEach(function(key) {
-    self.blueprint[key].properties = self.blueprint[key] && self.blueprint[key].properties || {link: '#'}
-    self.blueprint[key].properties.link = $('#leaflet-feature-link-' + key).val();
-  });
-  return self.blueprint;
-};
-
-MapEditor.prototype.highlightArea = function(blueprint) {
-  var self = this;
-  self.map._layers[blueprint].setStyle(self.highlightStyle);
-}
-
-MapEditor.prototype.defaultArea = function(blueprint) {
-  var self = this;
-  self.map._layers[blueprint].setStyle(self.defaultStyle);
-}
 
 $(function() {
 
@@ -150,8 +13,12 @@ $(function() {
   var percent = $('.progress-meter');
   var status = $('#status');
   var $form = $('form');
-  var editor;
+  var editors = [];
 
+  $maps.each(function() {
+    var table = document.getElementById("navigation_maps-table-" + $(this).data('id'));
+    editors.push(new MapEditor(this, table));
+  });
   // if($map.length) {
     // editor = new MapEditor($map.data('image'), $map.data('blueprint'));
   // }
@@ -159,9 +26,10 @@ $(function() {
   $form.ajaxForm({
     url: $form.find('[name=action]').val(),
     beforeSerialize: function() {
-      if(editor) {
-        // $form.find('[name=blueprint]').val(JSON.stringify(editor.getBlueprint()));
-      }
+      editors.forEach(function(editor) {
+        console.log(editor, `#blueprints_${editor.id}_blueprint` );
+        $(`#blueprints_${editor.id}_blueprint`).val(JSON.stringify(editor.getBlueprint()));
+      });
     },
     beforeSend: function() {
         status.empty();
@@ -179,7 +47,7 @@ $(function() {
         bar.width(percentVal)
         percent.html(percentVal);
         // if($form.find('input[type=file]').val()) {
-          location.reload();
+          // location.reload();
         // }
     },
     complete: function(xhr) {
