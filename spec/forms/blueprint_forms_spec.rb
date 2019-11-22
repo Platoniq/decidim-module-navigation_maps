51be34bf0b921de2eb427b03ac99164c795b88fb
Ivan Vergés <ivan@platoniq.net>
2019-11-22 16:17:15 +0100

    fix basic tests

--- spec/forms/blueprint_forms_spec.rb
+++ spec/forms/blueprint_forms_spec.rb
@@ -3,18 +3,13 @@
 require "spec_helper"
 
 module Decidim::NavigationMaps
-  describe BlueprintForm do
+  describe BlueprintForms do
     subject { described_class.from_params(attributes).with_context(context) }
 
     let(:organization) { create :organization }
-    let(:title) { Decidim::Faker::Localized.sentence(2) }
     let(:attributes) do
       {
-        title: title,
-        blueprint: {
-          "x" => "x data",
-          "y" => "y data"
-        }
+        "blueprints" => []
       }
     end
     let(:context) do
@@ -25,11 +20,6 @@ module Decidim::NavigationMaps
 
     context "when everything is OK" do
       it { is_expected.to be_valid }
-    end
-
-    context "when there is no title" do
-      let(:title) { nil }
-      it { is_expected.not_to be_valid }
     end
   end
 end
