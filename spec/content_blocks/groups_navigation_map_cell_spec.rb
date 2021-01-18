120c085994d346b197676580f3c1e92facd8a37f
Eduardo Martínez <eduardomech@gmail.com>
2021-01-18 13:41:03 +0100

    Add test for groups_navigation_map cell

---
+++ spec/content_blocks/groups_navigation_map_cell_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim::NavigationMaps::ContentBlocks
+  describe GroupsNavigationMapCell, type: :cell do
+    subject { cell(group_content_block.cell, group_content_block).call }
+
+    let(:organization) { create(:organization) }
+    let(:group_content_block) do
+      create(
+        :content_block,
+        organization: organization,
+        manifest_name: :navigation_map,
+        scope_name: :participatory_process_group_homepage
+      )
+    end
+    let(:organization_homepage_content_block) do
+      create(
+        :content_block,
+        organization: organization,
+        manifest_name: :navigation_map,
+        scope_name: :homepage
+      )
+    end
+    let!(:group_blueprint) { create(:blueprint, organization: organization, content_block: group_content_block) }
+    let!(:organization_homepage_blueprint) { create(:blueprint, organization: organization, content_block: organization_homepage_content_block) }
+
+    controller Decidim::PagesController
+
+    context "when there are blueprints in the content block" do
+      it "contains the map" do
+        expect(subject.to_s).to include(group_blueprint.image.url)
+        expect(subject.to_s).not_to include(organization_homepage_blueprint.image.url)
+      end
+    end
+  end
+end
