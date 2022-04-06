289310747496934dc21776400bde63a85afd0bc9
Ivan Vergés <ivan@platoniq.net>
2022-04-06 18:42:22 +0200

    remove unused image dir

--- lib/decidim/navigation_maps/version.rb
+++ lib/decidim/navigation_maps/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   # This holds the decidim-meetings version.
   module NavigationMaps
-    VERSION = "1.3.2"
+    VERSION = "1.3.3"
     DECIDIM_VERSION = [">= 0.25", "< 0.27"].freeze
   end
 end
