a24880d9cd31fdec134f3ae41baa208b59856176
Francisco Bolívar <francisco.bolivar@nazaries.com>
2023-02-14 14:34:12 +0100

    Upgrade to 0.27

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source "https://rubygems.org"
 
 ruby RUBY_VERSION
 # DECIDIM_VERSION =  { git: "https://github.com/decidim/decidim" }
-DECIDIM_VERSION = "0.26.1"
+DECIDIM_VERSION = "0.27.2"
 
 gem "decidim", DECIDIM_VERSION
 
--- lib/decidim/navigation_maps/version.rb
+++ lib/decidim/navigation_maps/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   # This holds the decidim-meetings version.
   module NavigationMaps
-    VERSION = "1.3.4"
-    DECIDIM_VERSION = [">= 0.25", "< 0.27"].freeze
+    VERSION = "1.4.0"
+    DECIDIM_VERSION = [">= 0.25", "< 0.28"].freeze
   end
 end
